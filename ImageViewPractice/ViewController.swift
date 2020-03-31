34ad54dd69f59e9b11d376bc35fd7ed8d8534a5b
Jon Villasanta <evillasanta@mail.sfsu.edu>
2020-03-30 18:27:39 -0700

    to be continued after my Mobile lecture class 03302020

--- ImageViewPractice/ViewController.swift
+++ ImageViewPractice/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        messageLabel.text = "YOu Are Awesome"
+        
+    }
+    
 }
 
